67aa7d0cfd69f420b3f07bc45ae10ec5692dd88e
Sirspam <71392316+Sirspam@users.noreply.github.com>
2022-03-15 00:11:53 +0000

    Added config check for bad AutoNyaWait value
    Somehow this value got set to zero in my config 💀

--- Nya/Configuration/PluginConfig.cs
+++ Nya/Configuration/PluginConfig.cs
@@ -64,10 +64,14 @@ namespace Nya.Configuration
         /// <remark>
         /// May have to make this check more than just the count in the future but for now this works
         /// Let's pray that the user never dare tampers with the config otherwise values in the SelectedEndpoints will never fix themselves
-        /// Enums? I hardly know them!
         /// </remark>
         private void FixConfigIssues()
         {
+            if (AutoNyaWait < 4)
+            {
+                AutoNyaWait = 4;
+            }
+            
             if (SelectedEndpoints.Count == ImageSources.Sources.Count)
             {
                 return;
