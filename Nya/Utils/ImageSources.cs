4e7cfca1d97bd7d9266a83000a1df8abb91e6e0d
Sirspam <71392316+Sirspam@users.noreply.github.com>
2023-01-28 12:55:41 +0000

    Added BocchiTheAPI

--- Nya/Utils/ImageSources.cs
+++ Nya/Utils/ImageSources.cs
@@ -60,6 +60,21 @@ namespace Nya.Utils
                 }
             },
             {
+                "BocchiTheAPI", new SourceData
+                {
+                    BaseEndpoint = "boccher.pixelboom.dev/api/",
+                    Mode =  DataMode.Json,
+                    SfwEndpoints = new List<string>
+                    {
+                        "frames", "frames?episode=1", "frames?episode=2", "frames?episode=3", "frames?episode=4",
+                        "frames?episode=5", "frames?episode=6", "frames?episode=7", "frames?episode=8",
+                        "frames?episode=9", "frames?episode=10", "frames?episode=11", "frames?episode=12",
+                        "frames?episode=OP", "frames?episode=ED1", "frames?episode=ED2", "frames?episode=ED3"    
+                    },
+                    NsfwEndpoints = new List<string>()
+                }
+            },
+            {
                 "Local Files", new SourceData
                 {
                     BaseEndpoint = Path.Combine(UnityGame.UserDataPath, "Nya"),
