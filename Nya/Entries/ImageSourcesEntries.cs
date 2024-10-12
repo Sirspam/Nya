c9153786f02703dfbe8c5062ea5fda22f9b58f7c
Sirspam <71392316+Sirspam@users.noreply.github.com>
2024-10-12 21:58:16 +0100

    Updated ImageSources schema
    Idk if schema is the correct word tehe

--- Nya/Entries/ImageSourcesEntries.cs
+++ Nya/Entries/ImageSourcesEntries.cs
@@ -5,15 +5,40 @@ namespace Nya.Entries
 {
     internal class ImageSourceEntry
     {
+        internal enum ResponseTypeEnum
+        {
+            Url,
+            Image
+        }
+        
+        /// <summary>
+        /// The URL to request the image from
+        /// </summary>
         [JsonProperty("Url")]
         public string Url { get; set; } = null!;
 
-        public bool IsLocal { get; set; } = false;
+        /// <summary>
+        /// The expected response when requesting an image
+        /// (URL to image or the image itself)
+        /// </summary>
+        [JsonProperty("ResponseType")]
+        public ResponseTypeEnum ResponseType { get; set; }
         
+        /// <summary>
+        /// List of SFW endpoints which the API supports
+        /// </summary>
         [JsonProperty("SfwEndpoints")]
         public List<string> SfwEndpoints { get; set; } = null!;
 
+        /// <summary>
+        /// List of NSFW endpoints which the API supports
+        /// </summary>
         [JsonProperty("NsfwEndpoints")]
         public List<string> NsfwEndpoints { get; set; } = null!;
+        
+        /// <summary>
+        /// Indicates if the source is saved locally on the user's machine.
+        /// </summary>
+        public bool IsLocal { get; set; } = false;
     }
 }
